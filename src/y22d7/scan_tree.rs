829ba44b5558099f1ce2057f66a08b46b30916fc
whiteand <andrewbeletskiy@gmail.com>
2022-12-07 20:18:43 +0200

    increased performance 5x

---
+++ src/y22d7/scan_tree.rs
@@ -0,0 +1,39 @@
+use super::io::ChangeDirArgument;
+
+use super::io::Node;
+
+use super::tree::Tree;
+
+use super::io::IO;
+
+pub(crate) fn scan_tree<'input>(ios: impl Iterator<Item = IO<'input>>) -> Tree<'input> {
+    let mut tree = Tree::default();
+    let mut current_path = Vec::new();
+    for io in ios {
+        match io {
+            IO::List(children) => {
+                for child in children {
+                    match child {
+                        Node::Directory(d) => {
+                            let mut full_path = current_path.clone();
+                            full_path.push(d);
+                            tree.add_directory(&full_path);
+                        }
+                        Node::File(f) => tree.add_file(&current_path, f),
+                    }
+                }
+            }
+            IO::ChangeDir(arg) => match arg {
+                ChangeDirArgument::Root => current_path.clear(),
+                ChangeDirArgument::Parent => {
+                    current_path.pop().unwrap();
+                }
+                ChangeDirArgument::Directory(d) => {
+                    current_path.push(d);
+                    tree.add_directory(&current_path);
+                }
+            },
+        }
+    }
+    tree
+}
---
+++ src/y22d7/tree.rs
@@ -0,0 +1,176 @@
+use super::io::File;
+
+#[derive(Debug)]
+pub enum Tree<'input> {
+    Root(Vec<Tree<'input>>),
+    Directory(&'input str, Vec<Tree<'input>>),
+    File(File<'input>),
+}
+
+impl<'input> Default for Tree<'input> {
+    fn default() -> Self {
+        Tree::Root(Vec::default())
+    }
+}
+
+impl<'input> Tree<'input> {
+    pub fn is_dir(&self) -> bool {
+        match self {
+            Tree::Root(_) => false,
+            Tree::Directory(_, _) => true,
+            Tree::File(_) => false,
+        }
+    }
+    pub fn has_name(&self, name: &str) -> bool {
+        match self {
+            Tree::Root(_) => false,
+            Tree::Directory(dir_name, _) => *dir_name == name,
+            Tree::File(f) => f.has_name(name),
+        }
+    }
+    pub fn add_file(&mut self, path: &[&'input str], f: File<'input>) {
+        if path.is_empty() {
+            match self {
+                Tree::Root(children) => {
+                    children.push(Tree::File(f));
+                }
+                Tree::Directory(_, children) => {
+                    children.push(Tree::File(f));
+                }
+                Tree::File(_) => todo!(),
+            };
+            return;
+        }
+        let first_name = path[0];
+        let mut to_insert = Some(f);
+        match self {
+            Tree::Root(children) => {
+                for child in children {
+                    if child.has_name(first_name) {
+                        if let Some(f) = to_insert.take() {
+                            child.add_file(&path[1..], f);
+                        }
+                    }
+                }
+            }
+            Tree::Directory(_, children) => {
+                for child in children {
+                    if child.has_name(first_name) {
+                        if let Some(f) = to_insert.take() {
+                            child.add_file(&path[1..], f);
+                        }
+                    }
+                }
+            }
+            Tree::File(_) => {}
+        }
+    }
+    pub fn add_directory(&mut self, path: &[&'input str]) {
+        if path.is_empty() {
+            return;
+        }
+        if path.len() == 1 {
+            let dir_name = path[0];
+            return match self {
+                Tree::Root(children) => {
+                    if children
+                        .iter()
+                        .find(|child| child.has_name(dir_name) && child.is_dir())
+                        .is_some()
+                    {
+                        return;
+                    }
+                    children.push(Tree::Directory(dir_name, Vec::new()));
+                }
+                Tree::Directory(_, children) => {
+                    if children
+                        .iter()
+                        .find(|x| x.has_name(dir_name) && x.is_dir())
+                        .is_some()
+                    {
+                        return;
+                    }
+                    children.push(Tree::Directory(dir_name, Vec::new()));
+                }
+                Tree::File(_) => unreachable!(),
+            };
+        }
+        let first_name = path[0];
+        match self {
+            Tree::Root(elements) => {
+                let mut added = false;
+                for x in elements.iter_mut() {
+                    if !x.is_dir() {
+                        continue;
+                    }
+                    if !x.has_name(first_name) {
+                        continue;
+                    }
+                    added = true;
+                    x.add_directory(&path[1..]);
+                }
+                if !added {
+                    elements.push(Tree::Directory(first_name, Vec::new()));
+                }
+            }
+            Tree::Directory(_, children) => {
+                let mut added = false;
+                for x in children.iter_mut() {
+                    if !x.is_dir() {
+                        continue;
+                    }
+                    if !x.has_name(first_name) {
+                        continue;
+                    }
+                    added = true;
+                    x.add_directory(&path[1..]);
+                }
+                if !added {
+                    children.push(Tree::Directory(first_name, Vec::new()));
+                }
+            }
+            Tree::File(_) => todo!(),
+        }
+    }
+
+    pub fn total_size(&self) -> usize {
+        match self {
+            Tree::Root(dirs) => dirs.iter().fold(0, |a, b| a + b.total_size()),
+            Tree::Directory(_, children) => children.iter().fold(0, |a, b| a + b.total_size()),
+            Tree::File(f) => f.size,
+        }
+    }
+}
+
+pub struct TreeIter<'input> {
+    to_visit: Vec<&'input Tree<'input>>,
+}
+
+impl<'input> Iterator for TreeIter<'input> {
+    type Item = &'input Tree<'input>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.to_visit.is_empty() {
+            return None;
+        }
+        let new_visit = self.to_visit.pop().unwrap();
+        match new_visit {
+            Tree::Root(dirs) => self.to_visit.extend(dirs.iter()),
+            Tree::Directory(_, children) => self.to_visit.extend(children.iter()),
+            Tree::File(_) => {}
+        }
+        Some(new_visit)
+    }
+}
+
+impl<'input> IntoIterator for &'input Tree<'input> {
+    type Item = &'input Tree<'input>;
+
+    type IntoIter = TreeIter<'input>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        TreeIter {
+            to_visit: vec![self],
+        }
+    }
+}
